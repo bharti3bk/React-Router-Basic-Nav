e6c366c02d82c86fc6a9e11184a55408e89adede
bharti3bk <bharti3bk@gmail.com>
2019-07-22 14:01:58 -0700

    Added Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import { Home, About, Contact, Navigation } from './components'; 
+import {Route} from 'react-router-dom';
 
 const App = () => (
-  <div>
+  <div>  
+    <Route exact path = {`/`} component = {Home} ></Route>
+    <Route exact path = {`/about`} component = {About}></Route>
+    <Route exact path = {'/contact'} component = {Contact}></Route>
     <Navigation />
   </div>
 );
